963121c13c0b65729b600a64ed41ddcf164983a1
DeviZowl <devizowl@gmail.com>
2025-08-09 09:41:49 +0000

    Dockerized.

--- my-eyes-they-are-burning/src/App.tsx
+++ my-eyes-they-are-burning/src/App.tsx
@@ -10,80 +10,80 @@ import './App.css'
 function App() {
 
   const posts = [
-  {
-    text: "This is a falkkin posto hav some anime biddies or smth",
-    img: "../public/Luto_Edit.png",
-  },
-  {
-    text: "yo this stream is actually fire no cap",
-    img: "../public/Luto_Edit.png",
-  },
-  {
-    text: "coding at 3am hits different tbh",
-    img: "../public/Luto_Edit.png",
-  },
-  {
-    text: "when the code actually works on first try",
-    img: "../public/Luto_Edit.png",
-  },
-  {
-    text: "chat help me debug this spaghetti code",
-    img: "../public/Luto_Edit.png",
-  },
-  {
-    text: "another day another React component",
-    img: "../public/Luto_Edit.png",
-  },
-  {
-    text: "why does CSS always gotta be so weird",
-    img: "../public/Luto_Edit.png",
-  },
-  {
-    text: "streaming and coding is the vibe honestly",
-    img: "../public/Luto_Edit.png",
-  },
-  {
-    text: "this bug has been haunting me for hours",
-    img: "../public/Luto_Edit.png",
-  },
-  {
-    text: "TypeScript really be saving my life rn",
-    img: "../public/Luto_Edit.png",
-  },
-  {
-    text: "console.log debugging best debugging don't @ me",
-    img: "../public/Luto_Edit.png",
-  },
-  {
-    text: "when you finally understand useEffect",
-    img: "../public/Luto_Edit.png",
-  },
-  {
-    text: "git commit -m 'fixed stuff hopefully'",
-    img: "../public/Luto_Edit.png",
-  },
-  {
-    text: "npm install anxiety has entered the chat",
-    img: "../public/Luto_Edit.png",
-  },
-  {
-    text: "Comic Sans makes everything better prove me wrong",
-    img: "../public/Luto_Edit.png",
-  }
-]
+    {
+      text: "This is a falkkin posto hav some anime biddies or smth",
+      img: "/Luto_Edit.png",
+    },
+    {
+      text: "yo this stream is actually fire no cap",
+      img: "/Luto_Edit.png",
+    },
+    {
+      text: "coding at 3am hits different tbh",
+      img: "/Luto_Edit.png",
+    },
+    {
+      text: "when the code actually works on first try",
+      img: "/Luto_Edit.png",
+    },
+    {
+      text: "chat help me debug this spaghetti code",
+      img: "/Luto_Edit.png",
+    },
+    {
+      text: "another day another React component",
+      img: "/Luto_Edit.png",
+    },
+    {
+      text: "why does CSS always gotta be so weird",
+      img: "/Luto_Edit.png",
+    },
+    {
+      text: "streaming and coding is the vibe honestly",
+      img: "/Luto_Edit.png",
+    },
+    {
+      text: "this bug has been haunting me for hours",
+      img: "/Luto_Edit.png",
+    },
+    {
+      text: "TypeScript really be saving my life rn",
+      img: "/Luto_Edit.png",
+    },
+    {
+      text: "console.log debugging best debugging don't @ me",
+      img: "/Luto_Edit.png",
+    },
+    {
+      text: "when you finally understand useEffect",
+      img: "/Luto_Edit.png",
+    },
+    {
+      text: "git commit -m 'fixed stuff hopefully'",
+      img: "/Luto_Edit.png",
+    },
+    {
+      text: "npm install anxiety has entered the chat",
+      img: "/Luto_Edit.png",
+    },
+    {
+      text: "Comic Sans makes everything better prove me wrong",
+      img: "/Luto_Edit.png",
+    }
+  ]
   const [showModal, setShowModal] = useState(true);
   const [isLockedOut, setIsLockedOut] = useState(false);
 
   return (
-    <div style={{background: "red"}}>
-     {isLockedOut ? <HAHAYOUSUCKTRYAGAIN /> : null}
-       <h1 style={{color: "yellow", fontFamily: 'Comic Sans MS'}}>THE BEST VERSION OF TWEETER/X</h1>
-        <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
-          {showModal ? <EndRacism onClose={setShowModal} onLOCKOUT={setIsLockedOut}></EndRacism> : <></>}
-        </div>
-       {posts.map((post, i) => (
+    <div style={{ background: "red" }}>
+      {isLockedOut ? <HAHAYOUSUCKTRYAGAIN /> : null}
+      <h1 style={{ color: "yellow", fontFamily: 'Comic Sans MS' }}>THE BEST VERSION OF TWEETER/X</h1>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        {showModal ? <EndRacism onClose={setShowModal} onLOCKOUT={setIsLockedOut}></EndRacism> : <></>}
+      </div>
+      {posts.map((post, i) => (
         <Post key={i} text={post.text} img={post.img}></Post>
-       ))}
+      ))}
     </div>
   )
 }
--- my-eyes-they-are-burning/src/EndRacism.tsx
+++ my-eyes-they-are-burning/src/EndRacism.tsx
@@ -1,141 +1,141 @@
 import { useState } from "react";
 
 type endRacismProps = {
-    onClose: any;
-    onLOCKOUT: any;
+  onClose: any;
+  onLOCKOUT: any;
 }
 
-function EndRacism({ onClose, onLOCKOUT }: endRacismProps){
-    const disclaimer = "i wIlL n0T b3 RaCIst oN LutWEEt3r!1!1!1 i wIlL n0T b3 RaCIst oN LutWEEt3r!1!1!1i wIlL n0T b3 RaCIst oN LutWEEt3r!1!1!1i wIlL n0T b3 RaCIst oN LutWEEt3r!1!1!1i wIlL n0T b3 RaCIst oN LutWEEt3r!1!1!1i wIlL n0T b3 RaCIst oN LutWEEt3r!1!1!1";
-    // const disclaimer = "hiiii!";
-    const [input, setInput] = useState('');
-    const [position, setPosition] = useState(0);
-    const [happyLilAccidents, setHappyLilAccidents] = useState(0);
+function EndRacism({ onClose, onLOCKOUT }: endRacismProps) {
+  const disclaimer = "i wIlL n0T b3 RaCIst oN LutWEEt3r!1!1!1 i wIlL n0T b3 RaCIst oN LutWEEt3r!1!1!1i wIlL n0T b3 RaCIst oN LutWEEt3r!1!1!1i wIlL n0T b3 RaCIst oN LutWEEt3r!1!1!1i wIlL n0T b3 RaCIst oN LutWEEt3r!1!1!1i wIlL n0T b3 RaCIst oN LutWEEt3r!1!1!1";
+  // const disclaimer = "hiiii!";
+  const [input, setInput] = useState('');
+  const [position, setPosition] = useState(0);
+  const [happyLilAccidents, setHappyLilAccidents] = useState(0);
 
-    const handleInputChange = (e: any) => {
-        const newInput = e.target.value;
-        
-        // Prevent typing beyond disclaimer length
-        if (newInput.length > disclaimer.length) return;
-        
-        for(let i = 0; i < 100; i++){
-            console.log('heh');
-        }
+  const handleInputChange = (e: any) => {
+    const newInput = e.target.value;
 
-        // Check if user typed a new character or deleted
-        if (newInput.length > input.length) {
-            // New character typed
-            const typedChar = newInput[newInput.length - 1];
-            const expectedChar = disclaimer[newInput.length - 1];
-            const isCorrect = typedChar === expectedChar;
+    // Prevent typing beyond disclaimer length
+    if (newInput.length > disclaimer.length) return;
 
-            if (!isCorrect) {
-                setHappyLilAccidents(happyLilAccidents + 1);
-            }
-            
-            // Add CSS rule for this character (nth-child is 1-indexed)
-            const styleSheet = document.styleSheets[0];
-            const nthPosition = newInput.length; // Convert to 1-indexed
-            const rule = `.typing-text span:nth-child(${nthPosition}) { 
+    for (let i = 0; i < 100; i++) {
+      console.log('heh');
+    }
+
+    // Check if user typed a new character or deleted
+    if (newInput.length > input.length) {
+      // New character typed
+      const typedChar = newInput[newInput.length - 1];
+      const expectedChar = disclaimer[newInput.length - 1];
+      const isCorrect = typedChar === expectedChar;
+
+      if (!isCorrect) {
+        setHappyLilAccidents(happyLilAccidents + 1);
+      }
+
+      // Add CSS rule for this character (nth-child is 1-indexed)
+      const styleSheet = document.styleSheets[0];
+      const nthPosition = newInput.length; // Convert to 1-indexed
+      const rule = `.typing-text span:nth-child(${nthPosition}) { 
                 color: ${isCorrect ? 'blue' : 'red'} !important; 
                 background-color: ${isCorrect ? 'rgba(0,0,255,0.2)' : 'rgba(255,0,0,0.2)'} !important; 
             }`;
-            
-            styleSheet.insertRule(rule, styleSheet.cssRules.length);
-            setPosition(newInput.length);
-            
-        } else if (newInput.length < input.length) {
-            // Character(s) were deleted
-            const deletedCount = input.length - newInput.length;
-            
-            // Reset deleted characters using CSS rules
-            for (let i = 0; i < deletedCount; i++) {
-                const deletedCharIndex = newInput.length + i;
-                
-                const styleSheet = document.styleSheets[0];
-                const rule = `.typing-text span:nth-child(${deletedCharIndex + 1}) { 
+
+      styleSheet.insertRule(rule, styleSheet.cssRules.length);
+      setPosition(newInput.length);
+
+    } else if (newInput.length < input.length) {
+      // Character(s) were deleted
+      const deletedCount = input.length - newInput.length;
+
+      // Reset deleted characters using CSS rules
+      for (let i = 0; i < deletedCount; i++) {
+        const deletedCharIndex = newInput.length + i;
+
+        const styleSheet = document.styleSheets[0];
+        const rule = `.typing-text span:nth-child(${deletedCharIndex + 1}) { 
                     color: white !important; 
                     background-color: transparent !important; 
                 }`;
-                
-                styleSheet.insertRule(rule, styleSheet.cssRules.length);
-            }
-            
-            setPosition(newInput.length);
-        }
-        
-        setInput(newInput);
+
+        styleSheet.insertRule(rule, styleSheet.cssRules.length);
+      }
+
+      setPosition(newInput.length);
     }
 
-    const handleClick = () => {
-        happyLilAccidents >= 5 ? onLOCKOUT(true) : onClose(false);
-    }
-  
-    const disclaimerSpans = disclaimer.split('').map((char, index) => (
-        <span key={index}>{char}</span>
-    ));
+    setInput(newInput);
+  }
 
-    return (
+  const handleClick = () => {
+    happyLilAccidents >= 5 ? onLOCKOUT(true) : onClose(false);
+  }
+
+  const disclaimerSpans = disclaimer.split('').map((char, index) => (
+    <span key={index}>{char}</span>
+  ));
+
+  return (
+    <div style={{
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      width: '100vw',
+      height: '100vh',
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      zIndex: 1000,
+    }}>
+      <div style={{
+        background: 'linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet)',
+        width: '70%',
+        height: '600px',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        position: 'absolute',
+        zIndex: '100000',
+        padding: '20px',
+        boxSizing: 'border-box'
+      }}>
+        <p style={{
+          color: "white",
+          fontFamily: 'Comic Sans MS',
+          textShadow: '2px 2px 0px black',
+          fontSize: '40px',
+          userSelect: 'none',
+          wordBreak: 'break-word',
+          overflowWrap: 'break-word',
+          margin: 0,
+          marginBottom: '20px'
+        }}
+          className="typing-text">
+          {disclaimerSpans}
+        </p>
+
+        <input
+          value={input}  // Added this!
+          onChange={handleInputChange}
+          style={{
+            width: '100%',
+            padding: '10px',
+            fontSize: '16px',
+            fontFamily: 'Comic Sans MS',
+            boxSizing: 'border-box'
+          }}
+          onPaste={(e) => e.preventDefault()}
+        />
+
         <div style={{
-                position: 'fixed',
-                top: 0,
-                left: 0,
-                width: '100vw',
-                height: '100vh',
-                backgroundColor: 'rgba(0, 0, 0, 0.5)',
-                zIndex: 1000,
-                        }}>
-            <div style={{ 
-                        background: 'linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet)',
-                        width: '70%',
-                        height: '600px',
-                        top: '50%',
-                        left: '50%',
-                        transform: 'translate(-50%, -50%)',
-                        position: 'absolute',
-                        zIndex:'100000',
-                        padding: '20px',
-                        boxSizing: 'border-box'
-                        }}>
-                <p style={{
-                    color: "white", 
-                    fontFamily: 'Comic Sans MS',   
-                    textShadow: '2px 2px 0px black',
-                    fontSize:'40px',
-                    userSelect: 'none',
-                    wordBreak: 'break-word',
-                    overflowWrap: 'break-word',
-                    margin: 0,
-                    marginBottom: '20px'
-                }}
-                className="typing-text">
-                    {disclaimerSpans}
-                </p>
-                
-                <input 
-                    value={input}  // Added this!
-                    onChange={handleInputChange}
-                    style={{
-                        width: '100%',
-                        padding: '10px',
-                        fontSize: '16px',
-                        fontFamily: 'Comic Sans MS',
-                        boxSizing: 'border-box'
-                    }}
-                    onPaste={(e) => e.preventDefault()}
-                />
-                
-                <div style={{
-                    color: 'white',
-                    fontFamily: 'Comic Sans MS',
-                    marginTop: '10px'
-                }}>
-                    Progress: {position} / {disclaimer.length}
-                </div>
-                {position === disclaimer.length && input === disclaimer ? <button onClick={handleClick}>I AM NOT RACIST</button> : <></>}
-            </div>
+          color: 'white',
+          fontFamily: 'Comic Sans MS',
+          marginTop: '10px'
+        }}>
+          Progress: {position} / {disclaimer.length}
         </div>
-    )
+        {position === disclaimer.length && input === disclaimer ? <button onClick={handleClick}>I AM NOT RACIST</button> : <></>}
+      </div>
+    </div>
+  )
 }
 
 export default EndRacism;
